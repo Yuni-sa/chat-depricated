a8d32639fa5a0c3fd7c9e1fa5f84e9cfbdea3576
Yonatan Sason <yonatanxd7@gmail.com>
2022-08-11 21:20:20 +0300

    login, cookies and getting user information

--- React-Chat/src/App.js
+++ React-Chat/src/App.js
@@ -1,28 +1,16 @@
-import './App.css';
-import ChatBody from './Components/ChatBody'
-import MessageBox from './Components/MessageBox';
-import ChatHeader from './Components/ChatHeader';
+import "./App.css";
+import ChatBody from "./Components/ChatBody";
+import MessageBox from "./Components/MessageBox";
+import ChatHeader from "./Components/ChatHeader";
 import React, { useRef, useEffect, useState } from "react";
-import { ws, sendMessage } from './client.js';
-import $ from 'jquery';
-import Navbar from './Components/Navbar';
+import { ws, sendMessage } from "./client.js";
+import $ from "jquery";
+import Navbar from "./Components/Navbar";
 
 //temporary,intended for testing
-export var username = ''
-export const id = prompt("what's your id?")
-export const password = prompt("what's your password?")
-
-// login func - gets the cookie
-// fetch('http://localhost:4000/api/login', {
-//   method: 'POST',
-//   headers: { 'Content-Type': 'application/json' },
-//   credentials: 'include',
-//   body: JSON.stringify({
-//     id,
-//     password
-//   })
-// })
-
+export var username = "";
+export const id = prompt("what's your id?");
+export const password = prompt("what's your password?");
 
 // register func
 /*fetch('http://localhost:4000/api/register', {
@@ -35,30 +23,38 @@ export const password = prompt("what's your password?")
   })
 })*/
 
-
-console.log(username)
+console.log(username);
 
 //global array of the messages
-export var global_msgs = []
+export var global_msgs = [];
 
+const App = ({}) => {
 
-const App = ({ }) => {
+  // login func - gets the cookie
+  fetch("http://localhost:4000/api/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    credentials: "include",
+    body: JSON.stringify({
+      id,
+      password,
+    }),
+  });
 
   // get user func
   // useEffect(() => {
-  //   (
-  //     async () => {
-  //       const response = await fetch('http://localhost:4000/api/user', {
-  //         headers: { 'Content-Type': 'application/json' },
-  //         credentials: 'include',
-  //       })
-  //       const info = await response.json()
-  //       username = info.username
-  //     }
-  //   )();
+     (
+       async () => {
+         const response = await fetch('http://localhost:4000/api/user', {
+           headers: { 'Content-Type': 'application/json' },
+           credentials: 'include',
+         })
+         const info = await response.json()
+         username = info.username
+         console.log(username)
+       }
+     )();
   // });
-
-
 
   // messages array and setMessages function,
   //used only for updating the messages (awkward solution for now)
@@ -67,44 +63,39 @@ const App = ({ }) => {
   //reference to the last message, used for scrolling
   const bottomRef = useRef(null);
 
-
   useEffect(() => {
     ws.onmessage = function (e) {
       let data = JSON.parse(e.data);
       var msg = data.text;
       var time = data.time;
-      time = new Date(time)
+      time = new Date(time);
       time = time.getHours() + ":" + time.getMinutes();
       var user = data.username;
       // console.log(msg);
-      if ($.trim(msg) == '') {
+      if ($.trim(msg) == "") {
         console.log("oof");
         return false;
       }
 
-      setMessages(current => [
+      setMessages((current) => [
         ...current,
         { msg: msg, time: time, user: user },
-      ])
+      ]);
 
-      global_msgs.push({ msg: msg, time: time, user: user })
-    }
+      global_msgs.push({ msg: msg, time: time, user: user });
+    };
   });
-
 
   /*useEffect(() => {
     setMessages(messages)
   }, [messages]);*/
 
-
   return (
-
     <div className="App">
       {/*<ChatHeader></ChatHeader>*/}
       <ChatBody msgs={global_msgs} bottomRef={bottomRef}></ChatBody>
       <MessageBox></MessageBox>
     </div>
-
   );
-}
+};
 export default App;
