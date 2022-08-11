255bd7761d513dcf7b9b7a3e728da95fbbd39cfa
Yonatan Sason <yonatanxd7@gmail.com>
2022-08-11 04:56:31 +0300

    starting to integrate auth

--- React-Chat/src/App.js
+++ React-Chat/src/App.js
@@ -8,8 +8,34 @@ import $ from 'jquery';
 import Navbar from './Components/Navbar';
 
 //temporary,intended for testing
-export var username = prompt("what's your name?")
-export var id = prompt("what's your id?")
+export var username = ''
+export const id = prompt("what's your id?")
+export const password = prompt("what's your password?")
+
+// login func - gets the cookie
+// fetch('http://localhost:4000/api/login', {
+//   method: 'POST',
+//   headers: { 'Content-Type': 'application/json' },
+//   credentials: 'include',
+//   body: JSON.stringify({
+//     id,
+//     password
+//   })
+// })
+
+
+// register func
+/*fetch('http://localhost:4000/api/register', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    id,
+    username,
+    password
+  })
+})*/
+
+
 console.log(username)
 
 //global array of the messages
@@ -17,6 +43,22 @@ export var global_msgs = []
 
 
 const App = ({ }) => {
+
+  // get user func
+  // useEffect(() => {
+  //   (
+  //     async () => {
+  //       const response = await fetch('http://localhost:4000/api/user', {
+  //         headers: { 'Content-Type': 'application/json' },
+  //         credentials: 'include',
+  //       })
+  //       const info = await response.json()
+  //       username = info.username
+  //     }
+  //   )();
+  // });
+
+
 
   // messages array and setMessages function,
   //used only for updating the messages (awkward solution for now)
@@ -34,7 +76,7 @@ const App = ({ }) => {
       time = new Date(time)
       time = time.getHours() + ":" + time.getMinutes();
       var user = data.username;
-      console.log(msg);
+      // console.log(msg);
       if ($.trim(msg) == '') {
         console.log("oof");
         return false;
