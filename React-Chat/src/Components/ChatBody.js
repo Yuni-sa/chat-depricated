a78d71f7e33e37792f51ec7117159dd27f50988e
Yonatan Sason <yonatanxd7@gmail.com>
2022-08-13 05:04:34 +0300

    fixed wrong chat message side bug

--- React-Chat/src/Components/ChatBody.js
+++ React-Chat/src/Components/ChatBody.js
@@ -6,38 +6,34 @@ import $ from "jquery";
 import { username } from "../App";
 
 function ChatBody(chat) {
+  var mine = Boolean;
   useEffect(() => {
     chat.bottomRef.current.scrollIntoView({ behavior: "smooth" });
   });
 
+  const renderMsgSide = () =>
+    chat.msgs.map((message, index) => {
+      mine = false;
+      //TODO: determine by id's
+      if (message.user == username) {
+        mine = true;
+      }
+      return (
+        <Message
+          id={index}
+          isMine={mine}
+          key={index}
+          ref_={chat.bottomRef}
+          txt={message.msg}
+          time={message.time}
+          user={message.user}
+        ></Message>
+      );
+    });
+
   return (
     <div className="chat-body">
-      {chat.msgs.map((message, index) => {
-        //TODO: determine by id's
-        if (message.user != username)
-          return (
-            <Message
-              id={index}
-              key={index}
-              ref_={chat.bottomRef}
-              txt={message.msg}
-              time={message.time}
-              user={message.user}
-            ></Message>
-          );
-        else
-          return (
-            <Message
-              id={index}
-              isMine={true}
-              key={index}
-              ref_={chat.bottomRef}
-              txt={message.msg}
-              time={message.time}
-              user={message.user}
-            ></Message>
-          );
-      })}
+      {renderMsgSide()}
       <div ref={chat.bottomRef} />
     </div>
   );
